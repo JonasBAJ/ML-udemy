788ed8b0e24adf777dc7ed5e5462d8504320c495
Elzbieta <Elzbieta@macbooks-MacBook-Air.local>
2017-04-16 21:24:34 +0300

    Initial

---
+++ Data_Preprocessing/python/categorical_data.py
@@ -0,0 +1,59 @@
+# Data Preprocessing
+
+from __future__ import print_function
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+class Helpers(object):
+
+    @staticmethod
+    def print_matrix(matrix, f_round=0):
+        for r in matrix:
+            for c in r:
+                if isinstance(c, float): print (round(c, f_round), end='')
+                else: print(c, end='')
+            print()
+
+
+# Importing the dataset
+data_set = pd.read_csv('../Data.csv')
+X = data_set.iloc[:, :-1].values
+y = data_set.iloc[:, 3].values
+
+
+# Taking care of missing data
+X[:, 1:3] = Imputer(missing_values='NaN', strategy='median', axis=0)\
+    .fit(X[:, 1:3])\
+    .transform(X[:, 1:3])
+
+
+# Encoding the categorical Independent Variable
+X[:, 0] = LabelEncoder().fit_transform(X[:, 0])
+
+X = OneHotEncoder(categorical_features=[0])\
+    .fit_transform(X)\
+    .toarray()
+
+# Encoding the Dependent Variable
+y = LabelEncoder().fit_transform(y)
+
+# Separate variables into test and train sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Normalize variables
+normalizer = StandardScaler()
+X_train = normalizer.fit_transform(X_train)
+X_test = normalizer.fit(X_test)
+
+
+print(X_train)
+print(X_test)
+
+print(y_train)
+print(y_test)
